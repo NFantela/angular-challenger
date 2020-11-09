458bf3a6b03f6b6dc08a4192498a125ac07f45ca
Nikša Fantela <niksa@robotiq.ai>
2020-11-09 15:44:23 +0100

    initial setup & instalation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: environment.navRoutes.components,
+    pathMatch: 'full'
+  },
+  { path: environment.navRoutes.components, 
+    loadChildren: () => import('./features/components-directives/components-directives.module').then(m => m.ComponentsAndDirectivesModule) 
+  },
+  // { path: environment.navRoutes.multiSettings, 
+  //   loadChildren: () => import('./features/multi-settings/multi-settings.module').then(m => m.MultiSettingsModule) 
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: environment.navRoutes.home
+  // }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/components-directives/components-directives-home/components-directives-home.module.ts
+++ src/app/features/components-directives/components-directives-home/components-directives-home.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionHomeComponent } from '../../shared/components/section-home.component';
 import { SectionHomeModule } from '../../shared/components/section-home.module';
 
-const routes: Routes = [{ path: '', component: SectionHomeModule }];
+const routes: Routes = [{ path: '', component: SectionHomeComponent }];
 
 @NgModule({
     imports: [SectionHomeModule, RouterModule.forChild(routes)],
